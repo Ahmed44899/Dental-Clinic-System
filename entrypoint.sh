b987e77b5b08bcaab4d36a5fb0f01e8b9168dafe
Ahmed44899 <ahmed44899@gmail.com>
2026-06-29 15:51:56 +0300

    Auto-create superuser on deploy using env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 python manage.py collectstatic --noinput
-python manage.py createsuperuser
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists():
+    User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')
+"
+
 gunicorn dental_clinic.wsgi:application --bind 0.0.0.0:$PORT
